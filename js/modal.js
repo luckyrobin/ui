79391dbf3c010431ebf82a810c83a9d871cb86ae
luckyrobin <icestear@gmail.com>
2014-07-24 16:59:34 +0800

    add normal modal

--- js/modal.js
+++ js/modal.js
@@ -1,55 +1,52 @@
-(function ($) {
-    var Modal = function (element, options) {
-        this.options = options;
-        this.$element = $(element);
+(function($) {
+    var Modal = function(element, options) {
+            this.options = options;
+            this.$element = $(element);
 
-        this.init('modal', element, options)
-    }
-    /**
-     * 基本参数设置
-     * @param title 标题
-     * @param mode 弹窗模式  'normal'|'confirm'|'alert'
-     * @param content '文字文字' 弹窗的内容
-     * @param width 弹窗的宽度
-     * @param height 弹窗的高度
-     * @param okvalue 确认按钮文字
-     * @param ok 点击确定的回调
-     * @param cancelvalue 取消按钮文字
-     * @param cancel 点击取消的回调
-     * @param template 内容模板
-     */
+            this.init('modal', element, options)
+        }
+        /**
+         * 基本参数设置
+         * @param title 标题
+         * @param mode 弹窗模式  'normal'|'confirm'|'alert'
+         * @param content '文字文字' 弹窗的内容
+         * @param width 弹窗的宽度
+         * @param height 弹窗的高度
+         * @param okvalue 确认按钮文字
+         * @param ok 点击确定的回调
+         * @param cancelvalue 取消按钮文字
+         * @param cancel 点击取消的回调
+         * @param template 内容模板
+         */
 
     Modal.DEFAULTS = {
         title: '标题',
         mode: 'normal',
         content: '你确定么？',
-        width: 360,
+        width: 412,
         height: 190,
         closed: false,
         maskLayer: true,
         okvalue: '确认',
-        ok: function () {
-        },
+        ok: function() {},
         cancelvalue: '取消',
-        cancel: function () {
-        },
-        callback: function () {
-        },
-        template: "<div class='sppopwin'>" +
-            "<div class='sppopwin-out'>" +
-            "<div class='sppopwin-in'>" +
-            "<div class='sppopwin-title'>" +
+        cancel: function() {},
+        callback: function() {},
+        template: "<div class='yhdbk-popwin'>" +
+            "<div class='yhdbk-popwin-out'>" +
+            "<div class='yhdbk-popwin-in'>" +
+            "<div class='yhdbk-popwin-title'>" +
             "<h3></h3>" +
-            "<span class='sppopwin-close sppopwin-action-c'>关闭</span>" +
+            "<span class='yhdbk-popwin-close yhdbk-popwin-action-c'>关闭</span>" +
             "</div>" +
-            "<div class='sppopwin-content'></div>" +
+            "<div class='yhdbk-popwin-content'></div>" +
             "</div>" +
             "</div>" +
             "</div>"
     }
 
-    Modal.prototype.init = function (type, element, options) {
-        /*		if (options instanceof Array) {
+    Modal.prototype.init = function(type, element, options) {
+        /*      if (options instanceof Array) {
          for (var i = 0; i < options.length; i++) {
          this.stringDo(options[i]);
          };
@@ -64,31 +61,38 @@
             this.confirm();
         } else if (mode == 'alert') {
             this.alert();
+        } else {
+            this.normal();
         }
     }
 
-    Modal.prototype.alert = function () {
-        var toolbar = "<div class='text-center'>" + this.options.content + "</div><p class='text-center mt30'><button class='spbtn spbtn-sure sppopwin-action-s'>" + this.options.okvalue + "</button></p>";
+    Modal.prototype.alert = function() {
+        var toolbar = "<div class='text-center'>" + this.options.content + "</div><p class='yhdbk-popwin-bottom-toolbar'><button class='yhdbk-button-01 yhdbk-popwin-action-s'>" + this.options.okvalue + "</button></p>";
         this.pop(toolbar);
     }
 
-    Modal.prototype.confirm = function () {
-        var toolbar = "<div class='text-center'>" + this.options.content + "</div><p class='text-center mt30'><button class='spbtn spbtn-sure sppopwin-action-s'>" + this.options.okvalue + "</button><button class='spbtn spbtn-cancel sppopwin-action-c ml'>" + this.options.cancelvalue + "</button></p>";
+    Modal.prototype.confirm = function() {
+        var toolbar = "<div class='text-center'>" + this.options.content + "</div><p class='yhdbk-popwin-bottom-toolbar'><button class='yhdbk-button-01 yhdbk-popwin-action-s'>" + this.options.okvalue + "</button><button class='yhdbk-button-03 yhdbk-popwin-action-c ml'>" + this.options.cancelvalue + "</button></p>";
         this.pop(toolbar);
     }
 
-    Modal.prototype.stringDo = function (whatido) {
+    Modal.prototype.normal = function() {
+        var toolbar = "<div class='text-center'>" + this.options.content + "</div>";
+        this.pop(toolbar);
+    }
+
+    Modal.prototype.stringDo = function(whatido) {
         switch (whatido) {
             case "open":
                 break;
         }
     }
 
-    Modal.prototype.getDefaults = function () {
+    Modal.prototype.getDefaults = function() {
         return Modal.DEFAULTS
     }
 
-    Modal.prototype.getOptions = function (options) {
+    Modal.prototype.getOptions = function(options) {
         options = $.extend({}, this.getDefaults(), options)
 
         if (options.delay && typeof options.delay == 'number') {
@@ -101,18 +105,18 @@
         return options
     }
 
-    Modal.prototype.tip = function () {
+    Modal.prototype.tip = function() {
         return this.$tip = this.$tip || $(this.options.template)
     }
 
-    Modal.prototype.close = function () {
+    Modal.prototype.close = function() {
         var $tip = this.tip();
         $(".popup_mask").remove();
         $(".popup_iframe").remove();
         return $tip.remove();
     }
 
-    Modal.prototype.pop = function (toolbar) {
+    Modal.prototype.pop = function(toolbar) {
         var _this = this;
         var _popupWidth = _this.options.width;
         var _popupHeight = _this.options.height;
@@ -122,9 +126,13 @@
 
         var $tip = _this.tip();
         var setContent = toolbar;
-        $tip.find(".sppopwin-content").html(setContent).end().find(".sppopwin-title > h3").text(_this.options.title);
+        $tip.find(".yhdbk-popwin-content").html(setContent).end().find(".yhdbk-popwin-title > h3").text(_this.options.title);
 
-        $tip.css({"width": _this.options.width, "margin-left": -(_popupWidth / 2), top: (_winHeight - _popupHeight) / 2});
+        $tip.css({
+            "width": _this.options.width,
+            "margin-left": -(_popupWidth / 2),
+            top: (_winHeight - _popupHeight) / 2
+        });
         $("body").append($tip);
         if (_this.options.maskLayer) {
             if ($tip.nextAll('.popup_mask').length == 0) {
@@ -137,24 +145,23 @@
             }
         }
         _this.options.callback();
-
-        $tip.find(".sppopwin-action-c").on("click", function () {
+        $tip.find(".yhdbk-popwin-action-c").on("click", function() {
             var fn = _this.options.cancel;
             return typeof fn !== 'function' || fn.call(this) !== false ?
                 _this.close().remove() : this;
         });
-        $tip.find(".sppopwin-action-s").on("click", function () {
+        $tip.find(".yhdbk-popwin-action-s").on("click", function() {
             var fn = _this.options.ok;
             return typeof fn !== 'function' || fn.call(this) !== false ?
                 _this.close().remove() : this;
         });
     }
 
-    $.fn.spmodal = function (option) {
+    $.fn.spmodal = function(option) {
         var options = typeof option == 'object' && option;
-        return this.each(function () {
+        return this.each(function() {
             var $this = $(this)
-            //new Modal(this, options);
+                //new Modal(this, options);
             if (typeof option == 'string') {
                 new Modal(this, [option]);
             } else {
@@ -181,4 +188,7 @@
  2014/06/12
  1.可控制弹窗是否关闭
  2.修复遮罩bug
+
+ 2014/07/24
+ 1.加入normal无toolbar模式
  */
